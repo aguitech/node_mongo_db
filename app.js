c7b2ea5a3369ac5e40a9b3cfb171b999c653db4f
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2020-10-19 19:27:37 -0500

    Instalando nodemon

--- app.js
+++ app.js
@@ -3,10 +3,24 @@ const path = requier('path');
 const bodyParser = require('body-parser');
 const app = express();
 
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+const mongo_uri = 'mongodb://dev:dev@localhost/todos';
+
+mongoose.connect(mongo_uri, function(err){
+  if(err){
+    throw err;
+  }else{
+    console.log(`Succesfully connected to ${mongo_uri}`)
+  }
+});
 
 app.get('/', (req, res) =>{
 
